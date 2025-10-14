d2d70fbff2646aeefebb0c4928a76eb975e27bc7
MaZen <MazenAnter>
2025-10-14 21:49:18 +0300

    - added adopt me button - finished pet details screen ui

--- lib/features/home/presentation/ui/pet_details_screen.dart
+++ lib/features/home/presentation/ui/pet_details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pet_finder_app/features/home/presentation/ui/widgets/details/adopt_me_button.dart';
 import 'package:pet_finder_app/features/home/presentation/ui/widgets/details/pet_details_content.dart';
 import 'package:pet_finder_app/features/home/presentation/ui/widgets/details/pet_details_sliver_app_bar.dart';
 import 'package:pet_finder_app/features/home/presentation/ui/widgets/favourite_button.dart';
@@ -18,6 +20,15 @@ class PetDetailsScreen extends StatelessWidget {
               PetDetailsSliverAppBar(pet: pet),
               SliverToBoxAdapter(child: PetDetailsContent(pet: pet)),
             ],
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 39.w, vertical: 20.h),
+              child: AdoptMeButton(),
+            ),
           ),
         ],
       ),
