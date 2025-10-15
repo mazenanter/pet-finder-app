aa4b7efa3107142fc40ebac99ec492614ba11b0f
MaZen <MazenAnter>
2025-10-15 19:26:27 +0300

    - added favorite request body & response

---
+++ lib/features/favourite/data/models/favorite_response.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'favorite_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+FavoriteResponse _$FavoriteResponseFromJson(Map<String, dynamic> json) =>
+    FavoriteResponse(
+      message: json['message'] as String,
+      id: (json['id'] as num).toInt(),
+    );
